0dbb831e86f655a4eed95884995acaee54fc8753
Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
2017-06-25 21:53:48 +0000

    Docs update.

--- files.js
+++ files.js
@@ -3,6 +3,6 @@ var files =
     [ "README.md", "_r_e_a_d_m_e_8md.html", null ],
     [ "src/config.php", "config_8php.html", "config_8php" ],
     [ "src/index.php", "index_8php.html", "index_8php" ],
-    [ "src/my.class.php", "my_8class_8php.html", "my_8class_8php" ],
-    [ "src/your.class.php", "your_8class_8php.html", "your_8class_8php" ]
+    [ "src/classes/my.class.php", "my_8class_8php.html", "my_8class_8php" ],
+    [ "src/classes/your.class.php", "your_8class_8php.html", "your_8class_8php" ]
 ];
